5fc3ab3220b051516c4dea206963f5359f4ffa18
jeiyanet <jeiya5934@proton.me>
2025-08-23 22:20:47 +0800

    build: add github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jeiyanet.github.io',
+  base: 'tpl-houses',
   vite: {
     plugins: [tailwindcss()],  
   },
